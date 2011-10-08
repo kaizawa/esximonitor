885bbef12f54d5b6fbc7d69924d12e14fab49c7e
Kazuyoshi Aizawa <admin2@whiteboard.ne.jp>
2011-10-09 02:08:26 +0900

    add nbproject files to git repository

--- src/com/cafeform/esxi/esximonitor/Main.java
+++ src/com/cafeform/esxi/esximonitor/Main.java
@@ -1,15 +1,4 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.cafeform.esxi.esximonitor;
-
-import ch.ethz.ssh2.Connection;
-import ch.ethz.ssh2.Session;
-import ch.ethz.ssh2.StreamGobbler;
-//import com.cafeform.esxi.ESXiConnection;
-//import com.cafeform.esxi.ESXiConnectionFactory;
-//import com.cafeform.esxi.VM;
 
 import com.vmware.vim25.VirtualMachinePowerState;
 import com.vmware.vim25.mo.Folder;
@@ -299,48 +288,6 @@ public class Main extends JFrame implements ActionListener {
                 });
             }
         });
-    }
-
-    public void doTest(String[] args) {
-        try {
-            /* Create a connection instance */
-            Connection conn = new Connection(getHostname());
-            /* Now connect */
-            conn.connect();
-            /* Authenticate.
-             * If you get an IOException saying something like
-             * "Authentication method password not supported by the server at this stage."
-             * then please check the FAQ.
-             */
-            boolean isAuthenticated = conn.authenticateWithPassword(getUsername(), getPassword());
-            if (isAuthenticated == false) {
-                throw new IOException("Authentication failed.");
-            }
-            /* Create a session */
-            Session sess = conn.openSession();
-            sess.execCommand("vim-cmd vmsvc/getallvms");
-            /* This basic example does not handle stderr, which is sometimes dangerous
-             * (please read the FAQ).
-             */
-            InputStream stdout = new StreamGobbler(sess.getStdout());
-            BufferedReader br = new BufferedReader(new InputStreamReader(stdout));
-            while (true) {
-                String line = br.readLine();
-                if (line == null) {
-                    break;
-                }
-                System.out.println(line);
-            }
-            /* Show exit status, if available (otherwise "null") */
-            System.out.println("ExitCode: " + sess.getExitStatus());
-            /* Close this session */
-            sess.close();
-            /* Close the connection */
-            conn.close();
-        } catch (IOException e) {
-            e.printStackTrace(System.err);
-            System.exit(2);
-        }
     }
 
     public static ImageIcon getSizedImageIcon(String path) throws IOException {
