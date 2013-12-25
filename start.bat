34ebc40f497dc1103acc9d6a99aad3defc46489e
Kazuyoshi Aizawa <admin2@whiteboard.ne.jp>
2013-12-25 23:52:41 +0900

    mod: modify launcher scripts

--- start.bat
+++ start.bat
@@ -1,1 +1,1 @@
-java -cp lib\dom4j-1.6.1.jar;lib\vijava520110926.jar;dist\esximonitor.jar com.cafeform.esxi.esximonitor.Main
+java -cp lib\dom4j-1.6.1.jar;lib\vijava520110926.jar -jar dist\esximonitor.jar
